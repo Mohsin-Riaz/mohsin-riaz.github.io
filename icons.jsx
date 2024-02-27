ca7d6267d6d2da2048b5fcbbd3a021714a41b69d
Mohsin-Riaz <Mohsin-Riaz@users.noreply.github.com>
2024-02-27 23:07:18 +0000

    deploy: b86e8ea9043ba45f399326215f4a6a99e63e597a

---
+++ icons.jsx
@@ -0,0 +1,37 @@
+import { IoLogoNodejs, IoLogoReact, IoLogoSass } from 'react-icons/io5';
+import {
+    SiAmazonaws,
+    SiAwslambda,
+    SiBun,
+    SiExpress,
+    SiGo,
+    SiMongodb,
+    SiPostgresql,
+    SiPython,
+    SiRust,
+    SiTypescript,
+} from 'react-icons/si';
+
+const icons = {
+    node: (
+        <IoLogoNodejs key="node">
+            <span class="tooltiptext" style={{ width: '10px', height: '10px' }}>
+                Tooltip text
+            </span>
+        </IoLogoNodejs>
+    ),
+    react: <IoLogoReact key="react" />,
+    sass: <IoLogoSass key="sass" />,
+    'aws-s3': <SiAmazonaws key="aws-s3" />,
+    'aws-lambda': <SiAwslambda key="aws-lambda" />,
+    express: <SiExpress key="express" />,
+    mongo: <SiMongodb key="mongo" />,
+    postgres: <SiPostgresql key="postgres" />,
+    typescript: <SiTypescript key="typescript" />,
+    python: <SiPython key="SiPython" />,
+    go: <SiGo key="SiGo" />,
+    bun: <SiBun key="SiBun" />,
+    rust: <SiRust key="SiRust" />,
+};
+
+export default icons;
